03d52c732c78a432650cc067393c9b7318eb2402
Ranjith S <ranjithselvaraj04@gmail.com>
2024-09-16 10:29:32 +0530

    heading up

--- sample/src/App.jsx
+++ sample/src/App.jsx
@@ -8,8 +8,8 @@ function App() {
     <>
       <div className="h-screen w-screen flex justify-center items-center bg-pink-600">
         <div className="h-5/6 w-[30%] flex flex-col justify-center items-center bg-white rounded-md shadow-2xl">
-        <h1 className="font-bold flex flex-row gap-3" >FORM</h1>
           <form className="w-[80%] h-[100%] flex flex-col justify-center items-center gap-4">
+           <h1 className="font-bold flex flex-row" >FORM</h1>
             <input type="text" name="" id="" placeholder="Name" className="p-3 bg-[#987585] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-pink-500 rounded-md" />
             <input type="text" name="" id="" placeholder="email" className="p-3 bg-[#987585] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-pink-500 rounded-md" />
             <input type="number" name="" id="" placeholder="phone" className="p-3 bg-[#987585] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-pink-500 rounded-md" />
