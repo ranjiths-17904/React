4802e1c0b898bf9ad95c1db3a9867e44bc4f7fd3
Ranjith S <ranjithselvaraj04@gmail.com>
2024-09-18 10:46:52 +0530

    c

--- sample/src/Components/Navbar.jsx
+++ sample/src/Components/Navbar.jsx
@@ -3,16 +3,17 @@
 const Navbar = () =>
     {
         return(
-                <nav className='h-10 w-100 flex flex-row bg-pink-700'>
-                   <div className="text-blue-200 font-extrabold">Portfolio</div>
-                    <div className="flex">
-                        <ul className="flex space-x-10">
-                            <li className="text-blue-100 hover:outline">Home</li>
-                            <li className="text-blue-100 hover:outline">About</li>
-                            <li className="text-blue-100 hover:outline gap-11">Contact</li>
-                        </ul>
-                   </div>
-                </nav>
+        <nav className='h-16 w-full flex flex-row items-center bg-pink-800 px-6'>
+        <div className="text-blue-200 font-bold text-lg">Portfolio</div>
+        <div className="flex flex-grow justify-end">
+            <ul className="flex space-x-8">
+                <li className="text-blue-100 font-medium hover:text-white cursor-pointer active:outline-none focus:border-b-10  hover:bg-blue-400">Home</li>
+                <li className="text-blue-100 font-medium hover:text-white cursor-pointer">About</li>
+                <li className="text-blue-100 font-medium hover:text-white cursor-pointer">Contact</li>
+            </ul>
+        </div>
+</nav>
+
         )
     }
 
