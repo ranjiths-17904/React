a3ee31ecb224bbaf615543d99c0ec09ad1c944ec
Ranjith S <ranjithselvaraj04@gmail.com>
2024-09-24 22:03:11 +0530

    ,

--- sample/src/pages/Project.jsx
+++ sample/src/pages/Project.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import '../assets/css/Pro.css'; // Ensure that you have styles for the card component
+import '../assets/css/Pro.css';
 
 const projects = [
   {
@@ -99,7 +99,7 @@ const Project = () => {
                 rel="noopener noreferrer"
                 className="text-black hover:text-gray-800 transition-colors duration-300"
               >
-                <i className="fab fa-github fa-2x"></i>
+                <i className="fab fa-github fa-2x"></i> {/* GitHub Font Awesome icon */}
               </a>
 
               {/* Like Icon */}
@@ -107,9 +107,20 @@ const Project = () => {
                 onClick={() => handleLike(project.id)}
                 className="text-red-500 hover:text-red-700 transition-transform transform hover:scale-110 duration-300"
               >
-                <i className="fas fa-heart fa-2x"></i>
+                <i className="fas fa-heart fa-2x"></i> {/* Heart Font Awesome icon */}
                 <span className="ml-1 text-lg font-semibold">{likeCounts[project.id]}</span>
               </button>
+              <a
+          href={project.githubLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-black hover:text-gray-800 transition-colors duration-300"
+        >
+  <svg height="32" width="32" viewBox="0 0 16 16" version="1.1" aria-hidden="true" fill="currentColor">
+    <path fillRule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.01.08-2.1 0 0 .67-.21 2.2.82A7.68 7.68 0 0 1 8 4.86c.68.01 1.37.09 2.01.26 1.52-1.04 2.2-.82 2.2-.82.44 1.09.16 1.9.08 2.1.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+  </svg>
+</a>
+
             </div>
           </div>
         ))}
