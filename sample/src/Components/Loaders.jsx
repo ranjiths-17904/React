da84bbe2b87a7ac66bf70362ec836b8e4bcb286a
Ranjith S <ranjithselvaraj04@gmail.com>
2024-10-31 12:22:37 +0530

    Summa

--- sample/src/Components/Loaders.jsx
+++ sample/src/Components/Loaders.jsx
@@ -1,23 +1,8 @@
-import { useEffect } from "react";
+import "../assets/css/Loader.css";
 
 const Loader = () => {
-  useEffect(() => {
-    const timer = setTimeout(() => {
-     
-      window.location.href = "/sample"; 
-    }, 3000);
-
-    return () => clearTimeout(timer); 
-  }, []);
-
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="relative w-32 h-24">
-        <div className="absolute bottom-8 left-20 h-7 w-7 rounded-full bg-teal-600 animate-bounce"></div>
-        <div className="absolute right-0 top-0 h-2 w-11 rounded-md bg-gray-300 animate-loading-step"></div>
-        <div className="absolute right-0 top-0 h-2 w-11 rounded-md bg-gray-300 animate-loading-step delay-300"></div>
-      </div>
-    </div>
+    <div className="loader"></div>
   );
 };
 
--- sample/src/pages/Home.jsx
+++ sample/src/pages/Home.jsx
@@ -1,22 +1,41 @@
+// HomePage.js
+import { useState, useEffect } from "react";
 import Profile from "./Profile";
 import Skills from './Skills';
 import Projects from "./Project";
 import Contact from './Contact';
-// import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
 import About from './About';
+// import Loader from '../Components/Loaders';
 
 const HomePage = () => {
+  // const [loading, setLoading] = useState(true);
+
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setLoading(false);
+  //   }, Math.floor(Math.random() * 1000) + 3000); 
+
+  //   return () => clearTimeout(timer);
+  // }, []);
+
+  // if (loading) {
+  //   return (
+  //     <div className="flex items-center justify-center min-h-screen bg-black">
+  //       <Loader />
+  //     </div>
+  //   );
+  // }
+
   return (
     <div>
-      {/* <Navbar /> Uncomment this if Navbar should be visible */}
       
       <section id="profile">
         <Profile />
       </section>
       <section id="about">
-      <About />
-    </section>
+        <About />
+      </section>
       <section id="skills">
         <Skills />
       </section>
