35b3443911dbfe7c2dd9ad262be3633a694d2335
Ranjith S <ranjithselvaraj04@gmail.com>
2024-11-04 10:31:28 +0530

    Update Scrollup.jsx

--- sample/src/Components/Scrollup.jsx
+++ sample/src/Components/Scrollup.jsx
@@ -26,9 +26,9 @@ const ScrollToTopButton = () => {
     return (
         <button
             onClick={scrollToTop}
-            className={`fixed bottom-6 right-4 w-8 h-8 flex items-center justify-center text-slate-50 text-base bg-gradient-to-r from-rose-800 via-indigo-800 to-yellow-600 rounded-full shadow-xl transition-transform transform hover:scale-110 hover:shadow-slate-200 ${
+            className={`fixed bottom-6 right-3 w-8 h-8 flex items-center justify-center text-slate-50 text-base bg-gradient-to-r from-rose-800 via-indigo-800 to-yellow-600 rounded-full shadow-xl transition-transform transform hover:scale-110 hover:shadow-slate-200 opacity-85 ${
                 isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
-            } animate-bounce-once`}
+            } animate-bounce`}
             style={{
                 backgroundSize: "200% 200%",
                 animation: "gradientAnimation 5s ease infinite, bounce 1s ease infinite",
