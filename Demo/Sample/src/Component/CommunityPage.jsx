028eb9b3b9945bb3a81d1217bfdd0219c4f40912
Ranjith S <ranjithselvaraj04@gmail.com>
2025-01-23 23:10:39 +0530

    upd our works

--- Demo/Sample/src/Component/CommunityPage.jsx
+++ Demo/Sample/src/Component/CommunityPage.jsx
@@ -107,7 +107,7 @@ const CommunityPage = () => {
       <h3 className="text-lg font-semibold mb-2 text-purple-500 flex justify-between items-center">New Works <span className="text-purple-500 text-2xl font-bold">+</span></h3>
       <ul>
         <li className="bg-[#141313] p-3 mb-2 rounded text-blue-400 flex items-center">
-          <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg width="14" height="14" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg" className='ml-2'>
             <circle cx="6" cy="6" r="6" fill="#250DFF" />
           </svg>
           <div className="ml-6 text-base font-medium">
@@ -116,7 +116,7 @@ const CommunityPage = () => {
           </div>
         </li>
         <li className="bg-[#141313] p-3 mb-2 rounded text-blue-400 flex items-center">
-          <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg width="14" height="14" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg" className='ml-2'>
             <circle cx="6" cy="6" r="6" fill="#250DFF" />
           </svg>
           <div className="ml-6 text-base font-medium">
@@ -125,7 +125,7 @@ const CommunityPage = () => {
           </div>
         </li>
         <li className="bg-[#141313] p-3 mb-2 rounded text-blue-400 flex items-center">
-          <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg width="14" height="14" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg" className='ml-2'>
             <circle cx="6" cy="6" r="6" fill="#250DFF" />
           </svg>
           <div className="ml-6 text-base font-medium">
@@ -139,9 +139,10 @@ const CommunityPage = () => {
       <h3 className="text-lg font-semibold mb-2 text-purple-500 flex justify-between items-center">In Progress <span className="text-purple-500 text-2xl font-bold">+</span></h3>
       <ul>
         <li className="bg-[#141313] p-3 mb-2 rounded text-blue-400 flex items-center">
-          <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="6" cy="6" r="6" fill="#250DFF" />
-          </svg>
+        <svg width="24" height="25" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M10.0002 20.0833C8.74739 20.0833 7.56336 19.8316 6.44808 19.3282C5.3328 18.8247 4.359 18.1379 3.52666 17.2677C2.69433 16.3976 2.03739 15.3795 1.55583 14.2135C1.07427 13.0476 0.833496 11.8097 0.833496 10.5C0.833496 9.1743 1.07427 7.93261 1.55583 6.77495C2.03739 5.61728 2.69433 4.60305 3.52666 3.73224C4.359 2.86143 5.3328 2.17463 6.44808 1.67182C7.56336 1.16902 8.74739 0.917295 10.0002 0.916656V2.83332C7.96822 2.83332 6.23786 3.58018 4.80908 5.07391C3.3803 6.56763 2.66622 8.37632 2.66683 10.5C2.66744 12.6237 3.38183 14.4327 4.81 15.927C6.23816 17.4214 7.96822 18.1679 10.0002 18.1667C12.0321 18.1654 13.7625 17.4188 15.1912 15.927C16.62 14.4352 17.3341 12.6262 17.3335 10.5H19.1668C19.1668 11.8097 18.9261 13.0476 18.4445 14.2135C17.9629 15.3795 17.306 16.3979 16.4737 17.2687C15.6413 18.1395 14.6712 18.8263 13.5632 19.3291C12.4553 19.8319 11.2676 20.0833 10.0002 20.0833Z" fill="#9747FF"/>
+</svg>
+
           <div className="ml-6 text-base font-medium">
             Project Name
             <div className="text-sm text-gray-500 -mr-12">Project description in ...</div>
@@ -149,9 +150,10 @@ const CommunityPage = () => {
         </li>
         <ProgressBar />
         <li className="bg-[#141313] p-3 mb-2 rounded text-blue-400 flex items-center">
-          <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="6" cy="6" r="6" fill="#250DFF" />
-          </svg>
+        <svg width="24" height="25" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M10.0002 20.0833C8.74739 20.0833 7.56336 19.8316 6.44808 19.3282C5.3328 18.8247 4.359 18.1379 3.52666 17.2677C2.69433 16.3976 2.03739 15.3795 1.55583 14.2135C1.07427 13.0476 0.833496 11.8097 0.833496 10.5C0.833496 9.1743 1.07427 7.93261 1.55583 6.77495C2.03739 5.61728 2.69433 4.60305 3.52666 3.73224C4.359 2.86143 5.3328 2.17463 6.44808 1.67182C7.56336 1.16902 8.74739 0.917295 10.0002 0.916656V2.83332C7.96822 2.83332 6.23786 3.58018 4.80908 5.07391C3.3803 6.56763 2.66622 8.37632 2.66683 10.5C2.66744 12.6237 3.38183 14.4327 4.81 15.927C6.23816 17.4214 7.96822 18.1679 10.0002 18.1667C12.0321 18.1654 13.7625 17.4188 15.1912 15.927C16.62 14.4352 17.3341 12.6262 17.3335 10.5H19.1668C19.1668 11.8097 18.9261 13.0476 18.4445 14.2135C17.9629 15.3795 17.306 16.3979 16.4737 17.2687C15.6413 18.1395 14.6712 18.8263 13.5632 19.3291C12.4553 19.8319 11.2676 20.0833 10.0002 20.0833Z" fill="#9747FF"/>
+</svg>
+
           <div className="ml-6 text-base font-medium">
             Project Name
             <div className="text-sm text-gray-500 -mr-12">Project description in ...</div>
@@ -159,9 +161,10 @@ const CommunityPage = () => {
         </li>
         <ProgressBar />
         <li className="bg-[#141313] p-3  rounded text-blue-400 flex items-center">
-          <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="6" cy="6" r="6" fill="#250DFF" />
-          </svg>
+        <svg width="24" height="25" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M10.0002 20.0833C8.74739 20.0833 7.56336 19.8316 6.44808 19.3282C5.3328 18.8247 4.359 18.1379 3.52666 17.2677C2.69433 16.3976 2.03739 15.3795 1.55583 14.2135C1.07427 13.0476 0.833496 11.8097 0.833496 10.5C0.833496 9.1743 1.07427 7.93261 1.55583 6.77495C2.03739 5.61728 2.69433 4.60305 3.52666 3.73224C4.359 2.86143 5.3328 2.17463 6.44808 1.67182C7.56336 1.16902 8.74739 0.917295 10.0002 0.916656V2.83332C7.96822 2.83332 6.23786 3.58018 4.80908 5.07391C3.3803 6.56763 2.66622 8.37632 2.66683 10.5C2.66744 12.6237 3.38183 14.4327 4.81 15.927C6.23816 17.4214 7.96822 18.1679 10.0002 18.1667C12.0321 18.1654 13.7625 17.4188 15.1912 15.927C16.62 14.4352 17.3341 12.6262 17.3335 10.5H19.1668C19.1668 11.8097 18.9261 13.0476 18.4445 14.2135C17.9629 15.3795 17.306 16.3979 16.4737 17.2687C15.6413 18.1395 14.6712 18.8263 13.5632 19.3291C12.4553 19.8319 11.2676 20.0833 10.0002 20.0833Z" fill="#9747FF"/>
+</svg>
+
           <div className="ml-6 text-base font-medium">
             Project Name
             <div className="text-sm text-gray-500 -mr-12">Project description in ...</div>
@@ -174,27 +177,27 @@ const CommunityPage = () => {
       <h3 className="text-lg font-semibold mb-2 text-purple-500 flex justify-between items-center">Our Releases <span className="text-purple-500 text-2xl font-bold">+</span></h3>
       <ul>
         <li className="bg-[#141313] p-3 mb-2 rounded text-blue-400 flex justify-between items-center">
-          <div>
+          <div className='ml-4 text-base font-medium'>
             Project Name
-            <div className="text-sm text-gray-500">Project description in ...</div>
+            <div className="text-sm text-gray-500 -mr-12">Project description in ...</div>
+          </div>
+          <svg width="10" height="15" viewBox="0 0 10 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M3.90775 13.6687L9.19567 8.81248C9.38494 8.63901 9.5351 8.43297 9.63755 8.20615C9.74001 7.97932 9.79274 7.73617 9.79274 7.4906C9.79274 7.24504 9.74001 7.00188 9.63755 6.77506C9.5351 6.54823 9.38494 6.34219 9.19567 6.16873L3.90775 1.31248C2.6215 0.149976 0.416504 0.974977 0.416504 2.64373V12.3375C0.416504 14.025 2.6215 14.85 3.90775 13.6687Z" fill="#9747FF"/>
+          </svg>
+        </li>
+        <li className="bg-[#141313] p-4 mb-2 rounded text-blue-400 flex justify-between items-center ">
+          <div className='ml-4 text-base font-medium'>
+            Project Name
+            <div className="text-sm text-gray-500 -mr-12">Project description in ...</div>
           </div>
           <svg width="10" height="15" viewBox="0 0 10 15" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M3.90775 13.6687L9.19567 8.81248C9.38494 8.63901 9.5351 8.43297 9.63755 8.20615C9.74001 7.97932 9.79274 7.73617 9.79274 7.4906C9.79274 7.24504 9.74001 7.00188 9.63755 6.77506C9.5351 6.54823 9.38494 6.34219 9.19567 6.16873L3.90775 1.31248C2.6215 0.149976 0.416504 0.974977 0.416504 2.64373V12.3375C0.416504 14.025 2.6215 14.85 3.90775 13.6687Z" fill="#9747FF"/>
           </svg>
         </li>
         <li className="bg-[#141313] p-3 mb-2 rounded text-blue-400 flex justify-between items-center">
-          <div>
+          <div className='ml-4 text-base font-medium'>
             Project Name
-            <div className="text-sm text-gray-500">Project description in ...</div>
-          </div>
-          <svg width="10" height="15" viewBox="0 0 10 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M3.90775 13.6687L9.19567 8.81248C9.38494 8.63901 9.5351 8.43297 9.63755 8.20615C9.74001 7.97932 9.79274 7.73617 9.79274 7.4906C9.79274 7.24504 9.74001 7.00188 9.63755 6.77506C9.5351 6.54823 9.38494 6.34219 9.19567 6.16873L3.90775 1.31248C2.6215 0.149976 0.416504 0.974977 0.416504 2.64373V12.3375C0.416504 14.025 2.6215 14.85 3.90775 13.6687Z" fill="#9747FF"/>
-          </svg>
-        </li>
-        <li className="bg-[#141313] p-3 mb-2 rounded text-blue-400 flex justify-between items-center">
-          <div>
-            Project Name
-            <div className="text-sm text-gray-500">Project description in ...</div>
+            <div className="text-sm text-gray-500 -mr-12">Project description in ...</div>
           </div>
           <svg width="10" height="15" viewBox="0 0 10 15" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M3.90775 13.6687L9.19567 8.81248C9.38494 8.63901 9.5351 8.43297 9.63755 8.20615C9.74001 7.97932 9.79274 7.73617 9.79274 7.4906C9.79274 7.24504 9.74001 7.00188 9.63755 6.77506C9.5351 6.54823 9.38494 6.34219 9.19567 6.16873L3.90775 1.31248C2.6215 0.149976 0.416504 0.974977 0.416504 2.64373V12.3375C0.416504 14.025 2.6215 14.85 3.90775 13.6687Z" fill="#9747FF"/>
@@ -252,7 +255,7 @@ const CommunityPage = () => {
 </div>
 
 
-        <div className="mt-16">
+        <div className="mt-24">
           <h2 className="text-4xl font-bold mb-8 text-center"># Life At Motrent</h2>
           <div className="relative w-full max-w-5xl mx-auto">
             <div className="flex justify-center items-center mb-4">
